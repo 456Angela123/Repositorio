77fa00bc1041a5df2ce486fdbb0bcae3d146d6ae
Angela <angelacuamatzi11@email.com>
2024-12-07 14:25:18 -0600

    Validacion

---
+++ Validacion.py
@@ -0,0 +1,10 @@
+user = 'Angela'
+password = '2021'
+
+user1  = input('Ingresa tu nombre')
+password1 = input('Ingresa tu contraseña')
+if user1  == user and password == password1:
+    print('correcto')
+else:
+    print('incorrecto')
+
